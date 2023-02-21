450513fe120cf2a201eebe58262d3c005c004ede
m-mfp <106394751+m-mfp@users.noreply.github.com>
2023-02-21 16:51:47 -0300

    corrected

--- SSWRecon-bash_copy.sh
+++ SSWRecon-bash_copy.sh
@@ -1,67 +1,63 @@
 #!/bin/bash
-echo "Enter url: "
-read url
-while [ "$url" = "" ]
+# Requesting url and making sure there is an input
+echo "Enter url"
+echo "Example: https://businesscorp.com.br"
+read fuzzurl
+while [ "$fuzzurl" = "" ]
 do
-    echo "Example: random-url.com" 
-    echo "Try again: "
-    read url
+    echo "Need input, try again" 
+    echo "Example: https://businesscorp.com.br"
+    read fuzzurl
 done
 
+# Making url for ffuf
+post=$(echo $fuzzurl | cut -d "/" -f 3)
+pre=$(echo $fuzzurl | cut -d ":" -f 1)
+url=$pre://FUZZ.$post
+
+# Getting wordlist for ffuf
 echo ""
 echo "Enter wordlist for SUBDOMAIN discovery or just press enter for default wordlist: "
 echo "Default: /usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt"
 read wordlist1
-echo "Default set: ${wordlist1:=/usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt}"
 
+# Getting wordlist for feroxbuster
 echo ""
 echo "Enter wordlist for DIRECTORY discovery or just press enter for default wordlist: "
 echo "Default: /usr/share/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt"
 read wordlist2
-echo "Default set: ${wordlist2:=/usr/share/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt}"
 
+# Performing ffuf
 echo ""
-echo "Performing FFUF for subdomain discovery..."
+echo "------------------- Performing FFUF for subdomain discovery with ${wordlist2:=/usr/share/seclists/Discovery/Web-Content/raft-large-directories-lowercase.txt} -------------------"
+ffuf -w $wordlist1 -u $url -H "User-Agent: Mozilla/5.0 (X11;Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0" -t 4 -timeout 5 -p "1.0-2.0" -rate 2 -s -of csv -o .tmp-ffuf
 
-ffuf -w $wordlist1 -u http://FUZZ.$url -H "User-Agent: Mozilla/5.0 (X11;Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0" -mc all -fs 0 -t 10 -p "0.1-2.0" -s -o result-1 2>/dev/null 1>/dev/null
-
-sed -i 's/"url"/\n/g' result-1
-cat result-1 | cut -d '"' -f2 | grep "http" | grep -v "FUZZ" > .tmp1
-echo "http://$url" >> .tmp1
-
+# Performing feroxbuster with filtered results from ffuf
 echo ""
-echo "Performing FEROXBUSTER for directory discovery..."
+echo "------------------- Performing FEROXBUSTER for directory discovery with ${wordlist1:=/usr/share/seclists/Discovery/DNS/subdomains-top1million-110000.txt} -------------------"
+echo "This may take a loooong time..."
+echo ""
 
-for i in $(cat .tmp1);
+touch .tmp-ferox
+for u in $(tail -n +2 .tmp-ffuf | cut -d "," -f 2);
 do
-feroxbuster --url $i --thread 10 --scan-limit 1 --rate-limit 4 --random-agent --wordlist $wordlist2 --thorough --silent --insecure --redirects --output "result-2" 2>/dev/null 1>/dev/null;
-echo -ne ".";
+feroxbuster --url $u --wordlist $wordlist2 --threads 10 --scan-limit 1 --rate-limit 4 --random-agent --collect-extensions --collect-backups --collect-words --redirects --insecure --quiet 2>/dev/null --output ".tmp-ferox";
 done
+
+# Filtered results from feroxbuster
+cat .tmp-ferox | awk -F "c " '{print $2}' | awk 'NF>0' > .tmp3
+
 echo ""
 echo "Making final adjustments..."
 
-cat result-2 | awk 'NF>0' > .tmp2
-
 touch SSWRecon-results.txt
-echo "SUBDOMAINS" > SSWRecon-results.txt
-cat .tmp1 >> SSWRecon-results.txt
+echo "--------------------------------- DIRECTORY LISTING ---------------------------------" >> SSWRecon-results.txt
+cat .tmp-ferox | grep directory | cut -d " " -f 12 >> SSWRecon-results.txt
 echo "" >> SSWRecon-results.txt
-echo "DIRECTORY LISTING" >> SSWRecon-results.txt
-cat .tmp2 | grep "directory listing" | awk -F "listing: " '{print $2}' | cut -d "(" -f 1 >> SSWRecon-results.txt
-echo "" >> SSWRecon-results.txt
-echo "DIRECTORIES FOUND" >> SSWRecon-results.txt
-cat result-2 | grep -v "MSG" >> SSWRecon-results.txt
+echo "--------------------------------- DIRECTORIES FOUND ---------------------------------" >> SSWRecon-results.txt
+for h in $(tail -n +2 .tmp-ffuf | cut -d "," -f 2);do grep $h .tmp3 >> SSWRecon-results.txt;echo " " >> SSWRecon-results.txt;done
 
-echo ""
-echo "...and we are done! Here is what we found:"
-echo ""
-echo "SUBDOMAINS:"
-cat .tmp1
-echo ""
-echo "POSSIBLE DIRECTORY LISTING:"
-cat .tmp2 | grep "directory listing" | awk -F "listing: " '{print $2}' | cut -d "(" -f 1
-echo ""
+rm -rf .tmp*
 
-rm -rf .tmp* result-*
-
+echo "We are finally done!"
 echo "All results stored in SSWRecon-results"
